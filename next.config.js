2885ca867252676531af4ecd2745ec92b7df0043
Norbert Takacs <norbert.takacs@postnord.com>
2022-07-27 16:52:11 -0500

    skip checking linting and type validation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 const withOffline = require('next-offline')
 
 module.exports = withOffline({
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
+	typescript: {
+		// !! WARN !!
+		// Dangerously allow production builds to successfully complete even if
+		// your project has type errors.
+		// !! WARN !!
+		ignoreBuildErrors: true,
+	},
 	async headers() {
 		return [
 			{
